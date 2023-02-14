11e29f3da3f1ac351c6220a3d3e292f65613253f
Alberto Santos Arenillas <alberto.santos@sstrategy.io>
2023-02-14 17:50:35 +0100

    Adds main function.

--- ratascript.py
+++ ratascript.py
@@ -1,6 +1,7 @@
 import re
 import json
 from krita import *
+from PyQt5.QtWidgets import QDialog, QHBoxLayout, QLineEdit, QPushButton
 
 # Número máximo de intentos de obtener una combinación no generada previamente.
 MAX_ATTEMPTS_TO_GET_A_NEW_COMBINATION = 1000
@@ -116,27 +117,43 @@ class LayerName:
 
 
 # -----------------------------------------------------
-# print("hello galaxy")
 
-# d = Krita.instance().createDocument(512, 512, "Python test document", "RGBA", "U8", "", 120.0)
-# Krita.instance().activeWindow().addView(d)
-
-application = Krita.instance()
-currentDoc = application.activeDocument()
-
-kGroups = currentDoc.topLevelNodes()
-
-# for g in groups:
-#     print(g.name())
+def show_options_dialog():
     
-combination_counter = CombinationCounter(kGroups)
-print(combination_counter.to_string())
+    wLayout = QHBoxLayout()
+    collectionNameInput = QLineEdit()
+    collectionNameInput.setPlaceholderText("Collection name")
+    wLayout.addWidget(collectionNameInput)
 
-compatible = combination_counter.categories_are_compatible(
-        combination_counter.groups[2]['layers'][0]['cats'], 
-        combination_counter.groups[3]['layers'][2]['cats'])
+    w = QDialog()
+    w.setWindowTitle('New collection')
+    w.setLayout(wLayout)
 
-print('cat1: ' + combination_counter.groups[2]['layers'][0]['layer_name'])
-print('cat2: ' + combination_counter.groups[3]['layers'][2]['layer_name'])
+    w.exec_()
 
-print('compatible? ' + str(compatible))
+  
+def main():
+    application = Krita.instance()
+    currentDoc = application.activeDocument()
+
+    kGroups = currentDoc.topLevelNodes()
+
+    combination_counter = CombinationCounter(kGroups)
+
+    print(combination_counter.to_string())
+
+    show_options_dialog()
+
+
+    # Pruebas:
+    compatible = combination_counter.categories_are_compatible(
+            combination_counter.groups[2]['layers'][0]['cats'], 
+            combination_counter.groups[3]['layers'][2]['cats'])
+
+    print('cat1: ' + combination_counter.groups[2]['layers'][0]['layer_name'])
+    print('cat2: ' + combination_counter.groups[3]['layers'][2]['layer_name'])
+
+    print('compatible? ' + str(compatible))
+
+
+
